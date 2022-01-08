691fb9d929518c5010f42c4c906f93c0022ba3ec
PatrekGill <patrekGill@yahoo.com>
2022-01-08 15:37:38 -0700

    Finished basic Tag entity mapping

---
+++ JPAGameList/src/test/java/com/skilldistillery/mygamelist/entities/TagTest.java
@@ -0,0 +1,48 @@
+package com.skilldistillery.mygamelist.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TagTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Tag tag;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAGameList");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		tag = em.find(Tag.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		tag = null;
+	}
+
+	@Test
+	void test_tag_name_mapping() {
+		assertNotNull(tag);
+		assertEquals("RPG", tag.getName());
+	}
+
+}
