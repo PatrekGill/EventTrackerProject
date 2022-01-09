6ff10a83713c4c0e594c7894ade6f7730d6a11b9
PatrekGill <patrekGill@yahoo.com>
2022-01-08 21:50:35 -0700

    Finished basic GameComment entity mapping

---
+++ JPAGameList/src/main/java/com/skilldistillery/mygamelist/entities/GameComment.java
@@ -0,0 +1,185 @@
+package com.skilldistillery.mygamelist.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name="game_comment")
+public class GameComment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="updated_date_time")
+    @UpdateTimestamp
+    private LocalDateTime updateDateTime;
+	
+    @Column(name = "created_date_time")
+	@CreationTimestamp
+	private LocalDateTime createdDateTime;
+    
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+    
+    @ManyToOne
+    @JoinColumn(name="in_reply_to")
+    private GameComment replyToComment;
+    
+    @ManyToOne
+    @JoinColumn(name="game_id")
+    private Game game;
+    
+    @OneToMany
+    @JoinColumn(name="in_reply_to")
+    private List<GameComment> replies;
+
+    private boolean visible;
+    private String text;
+    
+    
+	/* ----------------------------------------------------------------------------
+		Constructors
+	---------------------------------------------------------------------------- */
+	public GameComment() {}
+	
+	
+	/* ----------------------------------------------------------------------------
+		Get/Set ID
+	---------------------------------------------------------------------------- */
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+	/* ----------------------------------------------------------------------------
+		Get/Set updateDateTime
+	---------------------------------------------------------------------------- */
+	public LocalDateTime getUpdateDateTime() {
+		return updateDateTime;
+	}
+	public void setUpdateDateTime(LocalDateTime updateDateTime) {
+		this.updateDateTime = updateDateTime;
+	}
+
+
+	/* ----------------------------------------------------------------------------
+		Get/Set createdDateTime
+	---------------------------------------------------------------------------- */
+	public LocalDateTime getCreatedDateTime() {
+		return createdDateTime;
+	}
+	public void setCreatedDateTime(LocalDateTime createdDateTime) {
+		this.createdDateTime = createdDateTime;
+	}
+	
+
+	/* ----------------------------------------------------------------------------
+		Get/Set User
+	---------------------------------------------------------------------------- */
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+	/* ----------------------------------------------------------------------------
+		Get/Set ReplyToComment
+	---------------------------------------------------------------------------- */
+	public GameComment getReplyToComment() {
+		return replyToComment;
+	}
+	public void setReplyToComment(GameComment replyToComment) {
+		this.replyToComment = replyToComment;
+	}
+
+
+	/* ----------------------------------------------------------------------------
+		Get/Set Game
+	---------------------------------------------------------------------------- */
+	public Game getGame() {
+		return game;
+	}
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+
+	/* ----------------------------------------------------------------------------
+		Get/Set Visibility
+	---------------------------------------------------------------------------- */
+	public boolean isVisible() {
+		return visible;
+	}
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
+
+	/* ----------------------------------------------------------------------------
+		Get/Set Text
+	---------------------------------------------------------------------------- */
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	/* ----------------------------------------------------------------------------
+		Get/Set Replies
+	---------------------------------------------------------------------------- */
+	public List<GameComment> getReplies() {
+		return replies;
+	}
+	public void setReplies(List<GameComment> replies) {
+		this.replies = replies;
+	}
+
+
+	/* ----------------------------------------------------------------------------
+		Misc
+	---------------------------------------------------------------------------- */
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameComment other = (GameComment) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "GameComment [id=" + id + ", user=" + user + ", game=" + game + "]";
+	}
+	
+
+}
---
+++ JPAGameList/src/test/java/com/skilldistillery/mygamelist/entities/GameCommentTest.java
@@ -0,0 +1,98 @@
+package com.skilldistillery.mygamelist.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameCommentTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private GameComment comment;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAGameList");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		comment = em.find(GameComment.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		comment = null;
+	}
+
+	@Test
+	void test_GameComment_text_mapping() {
+		assertNotNull(comment);
+		assertTrue(comment.getText().contains("Good"));
+	}
+	
+	@Test
+	void test_GameComment_visibile_mapping() {
+		assertNotNull(comment);
+		assertTrue(comment.isVisible());
+	}
+
+	@Test
+	void test_GameComment_user_mapping() {
+		assertNotNull(comment);
+		assertNotNull(comment.getUser());
+		assertEquals(1,comment.getUser().getId());
+	}
+	
+	@Test
+	void test_GameComment_replyToComment_mapping() {
+		comment = em.find(GameComment.class, 2);
+		assertNotNull(comment);
+		assertNotNull(comment.getReplyToComment());
+		assertEquals(1,comment.getReplyToComment().getId());
+	}
+	
+	@Test
+	void test_GameComment_game_mapping() {
+		assertNotNull(comment);
+		assertNotNull(comment.getGame());
+		assertEquals(1,comment.getGame().getId());
+	}
+	
+	@Test
+	void test_GameComment_replies_mapping() {
+		assertNotNull(comment);
+		assertNotNull(comment.getReplies());
+		assertFalse(comment.getReplies().isEmpty());
+		assertEquals(2,comment.getReplies().get(0).getId());
+	}
+
+	
+	@Test
+	void test_GameComment_createdDateTime_mapping() {
+		assertNotNull(comment);
+		assertEquals(1, comment.getCreatedDateTime().getMonthValue());
+		assertEquals(7, comment.getCreatedDateTime().getDayOfMonth());
+	}
+	
+	@Test
+	void test_GameComment_updatedDateTime_mapping() {
+		assertNotNull(comment);
+		assertEquals(1, comment.getUpdateDateTime().getMonthValue());
+		assertEquals(8, comment.getUpdateDateTime().getDayOfMonth());
+	}
+}
