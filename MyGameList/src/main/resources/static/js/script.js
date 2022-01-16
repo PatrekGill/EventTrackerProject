911c3f0246d1bf7ce3431a7df9df8bf702855cb0
PatrekGill <patrekGill@yahoo.com>
2022-01-16 09:37:12 -0700

    Integraed search eventhandler

--- MyGameList/src/main/resources/static/js/script.js
+++ MyGameList/src/main/resources/static/js/script.js
@@ -1,10 +1,21 @@
 console.log("script.js loaded");
 
+function init() {
+    console.log("Init Executed");
+    addSearchEvent();
+}
+
 window.addEventListener("load",function(event) {
     console.log("document loaded");
     init();
 });
 
-function init() {
-    console.log("Init Executed");
+
+var searchForGamesFunction = function(event) {
+    console.log("searched");
+}
+
+function addSearchEvent() {
+    let searchButton = document.getElementById("searchButton");
+    searchButton.addEventListener("click",searchForGamesFunction);
 }
