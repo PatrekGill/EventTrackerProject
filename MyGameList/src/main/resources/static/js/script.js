9fd441eab0af2336d08ca681a1293258d830a21d
PatrekGill <patrekGill@yahoo.com>
2022-01-16 10:04:20 -0700

    added basic api request for game search

--- MyGameList/src/main/resources/static/js/script.js
+++ MyGameList/src/main/resources/static/js/script.js
@@ -12,7 +12,29 @@ window.addEventListener("load",function(event) {
 
 
 var searchForGamesFunction = function(event) {
+    event.preventDefault();
     console.log("searched");
+
+    let searchValue = document.getElementById("searchInput").value;
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET",`api/games/search/${searchValue}`);
+    xhr.onreadystatechange = function() {
+        console.log("state change");
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log("Request Good");
+                let games = JSON.parse(xhr.responseText);
+                console.log(games);
+
+            } else {
+                console.log("Request bad");
+                console.log(xhr.status);
+            }
+        }
+    };
+
+    xhr.send();
 }
 
 function addSearchEvent() {
