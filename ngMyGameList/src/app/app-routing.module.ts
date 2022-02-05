f30b917c1bbd6c454f2800efc50ef06cad1a2233
PatrekGill <patrekGill@yahoo.com>
2022-02-05 16:25:34 -0700

    updated angualr front-end

--- ngMyGameList/src/app/app-routing.module.ts
+++ ngMyGameList/src/app/app-routing.module.ts
@@ -3,13 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { GamesComponent } from './components/games/games.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
-// import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'games', component: GamesComponent },
-  { path: '**', component: NotfoundComponent },
+  { path: '**', component: NotfoundComponent }
 ];
 
 @NgModule({
--- ngMyGameList/src/app/services/game.service.ts
+++ ngMyGameList/src/app/services/game.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Game } from '../models/game';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-  private baseUrl = 'http://localhost:8083/';
-  private url = this.baseUrl + 'api/games';
+  // private baseUrl = 'http://localhost:8083/';
+  private url = environment.baseUrl + 'api/games';
 
   constructor(
     private http: HttpClient
